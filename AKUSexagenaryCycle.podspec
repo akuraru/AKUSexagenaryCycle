f1b45e714c80e2062428f78e3480d4ae7b3d9ba1
akuraru <akuraru@gmail.com>
2017-12-14 22:15:39 +0900

    delete dependency

--- AKUSexagenaryCycle.podspec
+++ AKUSexagenaryCycle.podspec
@@ -21,11 +21,8 @@ Pod::Spec.new do |s|
   s.source           = { :git => "https://github.com/akuraru/AKUSexagenaryCycle.git", :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/akuraru'
 
-  s.platform     = :ios, '10.3.1'
+  s.platform     = :ios, '10.0'
   s.requires_arc = true
 
   s.source_files = 'Pod/Classes'
-  
-  s.dependency 'NSDate-Escort'
-  s.dependency 'AZDateBuilder'
 end
--- Pod/Classes/AKUSexagenaryCycle.m
+++ Pod/Classes/AKUSexagenaryCycle.m
@@ -3,37 +3,46 @@
 // Copyright (c) 2014 P.I.akura. All rights reserved.
 //
 
-#import <NSDate-Escort/NSDate+Escort.h>
-#import <AZDateBuilder/NSDate+AZDateBuilder.h>
 #import "AKUSexagenaryCycle.h"
-
 
 @implementation AKUSexagenaryCycle {
 
 }
 + (TenHeavenlyStems)stemsYearForDate:(NSDate *)date {
-    return (TenHeavenlyStems) (date.gregorianYear - 4) % 10;
+    NSCalendar *currentCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *components = [currentCalendar components:NSCalendarUnitEra | NSCalendarUnitYear fromDate:date];
+    return (TenHeavenlyStems) ([components year] - 4) % 10;
 }
 
 + (TwelveEarthlyBranches)branchesYearForDate:(NSDate *)date {
-    return (TwelveEarthlyBranches) (date.gregorianYear - 4) % 12;
+    NSCalendar *currentCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *components = [currentCalendar components:NSCalendarUnitEra | NSCalendarUnitYear fromDate:date];
+    return (TwelveEarthlyBranches) ([components year] - 4) % 12;
 }
 
 + (TenHeavenlyStems)stemsMonthForDate:(NSDate *)date {
-    return (TenHeavenlyStems) ((date.gregorianYear % 5 * 12) + date.month + 3) % 10;
+    NSCalendar *currentCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *components = [currentCalendar components:NSCalendarUnitEra | NSCalendarUnitYear | NSCalendarUnitMonth fromDate:date];
+    return (TenHeavenlyStems) (([components year] % 5 * 12) + [components month] + 3) % 10;
 }
 
 + (TwelveEarthlyBranches)branchesMonthForDate:(NSDate *)date {
-    return (TwelveEarthlyBranches) (date.month + 1) % 12;
+    NSCalendar *currentCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *components = [currentCalendar components:NSCalendarUnitEra | NSCalendarUnitYear | NSCalendarUnitMonth fromDate:date];
+    return (TwelveEarthlyBranches) ([components month] + 1) % 12;
 }
 
 + (TwelveEarthlyBranches)branchesDayForDate:(NSDate *)date {
-    NSDate *currentDate = [NSDate AZ_dateByUnit:@{
-            AZ_DateUnit.year : @2014,
-            AZ_DateUnit.month : @1,
-            AZ_DateUnit.day : @5,
-    }];
-    NSInteger days = [currentDate daysBeforeDate:date];
+    NSCalendar *currentCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *dateComponents = [[NSDateComponents alloc] init];
+    dateComponents.year = 2014;
+    dateComponents.month = 1;
+    dateComponents.day = 5;
+    NSDate *currentDate = [currentCalendar dateFromComponents:dateComponents];
+    
+    
+    NSDateComponents *components = [currentCalendar components:NSCalendarUnitDay fromDate:currentDate toDate:date options:0];
+    NSInteger days = [components day];
     NSInteger d = days % 12;
     return (TwelveEarthlyBranches) (d < 0 ? d + 12 : d);
 }
